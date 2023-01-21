eb6f1799a8dbe86dbe2751f18a5ed4595c33afcc
Dmutro4347 <dmutrobui@gmail.com>
2023-01-21 13:58:21 +0200

    commit

---
+++ convert.bat
@@ -0,0 +1,2 @@
+rem pyuic6 -o .\UI\design.py -x .\UI\design.ui
+pyuic6 -o .\UI\design_setup.py -x .\UI\design_setup.ui
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6 import QtGui
-from UI.MQTT_design import Ui_MainWindow
+from PyQt6 import QtGui, QtWidgets
+
+from UI.design import Ui_MainWindow
+from UI.design_setup import Ui_DialogSetup
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,6 +15,7 @@ class MainWindow(QMainWindow):
         self.ui.statusbar.showMessage('Я готовий')
         self.ui.pbtnOnOff.clicked.connect(self.on_clicked_onoff)
         self.ui.pbtnConnect.clicked.connect(self.on_clicked_connect)
+        self.ui.pbtnSettings.clicked.connect(self.on_clicked_settings)
 
     def on_clicked_onoff(self):
         if self.ui.pbtnOnOff.text() == 'Увімкнути':
@@ -27,6 +30,17 @@ class MainWindow(QMainWindow):
         else:
             self.ui.pbtnConnect.setText('Підключено')
             self.ui.lblIconConnect.setPixmap(QtGui.QPixmap('icon/checkYes.jpg'))
+
+    def on_clicked_settings(self):
+        self.ui.statusbar.showMessage('Settings')
+        DialogSetup = QtWidgets.QDialog()
+        ui = Ui_DialogSetup()
+        ui.setupUi(DialogSetup)
+        buttonClicked = DialogSetup.exec()
+        if buttonClicked:
+            self.ui.statusbar.showMessage('Нажали кнопку Save')
+        else:
+            self.ui.statusbar.showMessage('Нажали кнопку Cancel')
 
 
 if __name__ == "__main__":
